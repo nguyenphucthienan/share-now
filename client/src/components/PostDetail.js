b89c625578e9d91e5ff5516928bc1e5fbce37914
Nguyen Phuc Thien An <npta97@gmail.com>
2018-04-05 16:22:16 +0700

    Update PostDetail component

--- client/src/components/PostDetail.js
+++ client/src/components/PostDetail.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 import config from '../config';
-import { fetchPost, clearPost, fetchComments, clearComments } from '../actions';
+import {
+  fetchPosts,
+  fetchPost,
+  clearPost,
+  fetchComments,
+  clearComments
+} from '../actions';
 
 import CommentList from './CommentList';
 
@@ -15,9 +22,39 @@ class PostDetail extends Component {
     this.props.fetchComments(postId);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.post) {
+      const { _id: userId } = this.props.user;
+      const { hearts } = nextProps.post;
+
+      this.setState({
+        isPostHearted: hearts.includes(userId),
+        numOfHearts: hearts.length
+      });
+    }
+  }
+
   componentWillUnmount() {
     this.props.clearPost();
     this.props.clearComments();
+  }
+
+  async heartOrUnheartPost() {
+    if (this.state.isPostHearted) {
+      this.setState({
+        isPostHearted: !this.state.isPostHearted,
+        numOfHearts: this.state.numOfHearts - 1
+      });
+    } else {
+      this.setState({
+        isPostHearted: !this.state.isPostHearted,
+        numOfHearts: this.state.numOfHearts + 1
+      });
+    }
+
+    const { _id: postId } = this.props.post;
+    await axios.post(`/api/posts/${postId}/heart`, null);
+    await this.props.fetchPosts();
   }
 
   renderBackButton() {
@@ -37,8 +74,10 @@ class PostDetail extends Component {
     if (this.props.post) {
       const {
         _id: postId,
-        image, title, content, hearts
+        image, title, content
       } = this.props.post;
+
+      const heartColor = this.state.isPostHearted ? 'red' : 'grey darken-2';
 
       return (
         <div className="container">
@@ -52,14 +91,19 @@ class PostDetail extends Component {
               <div className="card row">
                 <div className="col s12 center-align image-detail">
                   <img className="responsive-img" src={image} alt={title} />
-                  <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">favorite</i></a>
+                  <a
+                    className={`btn-floating halfway-fab waves-effect waves-light ${heartColor}`}
+                    onClick={() => this.heartOrUnheartPost()}
+                  >
+                    <i className="material-icons">favorite</i>
+                  </a>
                 </div>
               </div>
               <div className="card row">
                 <div className="col s12">
                   <p>
                     <span className="card-content-title">Hearts: </span>
-                    <span>{hearts.length || 0}</span>
+                    <span>{this.state.numOfHearts}</span>
                   </p>
                   <p>
                     <span className="card-content-title">Content: </span>
@@ -79,11 +123,12 @@ class PostDetail extends Component {
   }
 }
 
-function mapStateToProps({ post }) {
-  return { post };
+function mapStateToProps({ user, post }) {
+  return { user, post };
 }
 
 export default connect(mapStateToProps, {
+  fetchPosts,
   fetchPost,
   clearPost,
   fetchComments,
