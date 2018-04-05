60d20a93503439758864f1ef6597e57fa0463fbf
Nguyen Phuc Thien An <npta97@gmail.com>
2018-04-05 15:57:53 +0700

    Update PostCard component

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
+import axios from 'axios';
 
 class PostCard extends Component {
+  constructor(props) {
+    super(props);
+    const { _id: userId } = this.props.user;
+    const { hearts } = this.props.post;
+
+    this.state = {
+      isPostHearted: hearts.includes(userId),
+      numOfHearts: hearts.length
+    };
+  }
+
+  async heartOrUnheartPost() {
+    if (this.state.isPostHearted) {
+      this.setState({
+        isPostHearted: !this.state.isPostHearted,
+        numOfHearts: this.state.numOfHearts - 1
+      });
+    } else {
+      this.setState({
+        isPostHearted: !this.state.isPostHearted,
+        numOfHearts: this.state.numOfHearts + 1
+      });
+    }
+
+    const { _id: postId } = this.props.post;
+    await axios.post(`/api/posts/${postId}/heart`, null);
+  }
+
   render() {
     const {
       _id: postId,
       image,
-      title,
-      content
+      title
     } = this.props.post;
+
+    const heartColor = this.state.isPostHearted ? 'red' : 'grey darken-2';
 
     return (
       <div className="col s12 m6 l4">
@@ -18,10 +49,17 @@ class PostCard extends Component {
             <Link to={`/posts/${postId}`}>
               <span className="card-title">{title}</span>
             </Link>
-            <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">favorite</i></a>
+            <a
+              className={`btn-floating halfway-fab waves-effect waves-light ${heartColor}`}
+              onClick={() => this.heartOrUnheartPost()}
+            >
+              <i className="material-icons">favorite</i></a>
           </div>
           <div className="card-content">
-            <p>{content}</p>
+            <p>
+              <span className="card-content-title">Hearts: </span>
+              <span>{this.state.numOfHearts}</span>
+            </p>
           </div>
         </div>
       </div>
@@ -29,4 +67,8 @@ class PostCard extends Component {
   }
 }
 
-export default withRouter(PostCard);
+function mapStateToProps({ user }) {
+  return { user };
+}
+
+export default connect(mapStateToProps)(withRouter(PostCard));
