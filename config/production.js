93463c052d97337529226dc44027658d977bc364
Nguyen Phuc Thien An <npta97@gmail.com>
2018-04-25 11:09:04 +0700

    Update Server Config

--- config/production.js
+++ config/production.js
@@ -3,5 +3,7 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  vapidPublicKey: process.env.VAPID_PUBLIC_KEY,
+  vapidPrivateKey: process.env.VAPID_PRIVATE_KEY
 };
