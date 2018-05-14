5a1e46fa7e971de3246c5c067bdf68bf579e5c46
Nguyen Si Dat <15520105@gm.uit.edu.vn>
2018-05-14 11:00:05 +0700

    Restructure project

--- client/src/config/production.js
+++
@@ -1,7 +0,0 @@
-export default {
-  appName: process.env.APP_NAME,
-  baseURL: process.env.BASE_URL,
-  uploadImageURL: process.env.UPLOAD_IMAGE_URL,
-  googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
-  vapidPublicKey: process.env.VAPID_PUBLIC_KEY
-};
